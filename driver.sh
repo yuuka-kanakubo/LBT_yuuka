ba00c31ad95acaf216f0e1badd8475e765728915
Yuuka Kanakubo <yuuka.kanakubo@gmail.com>
2025-02-15 09:14:27 +0900

    Small adjustments to riken cluster 02

--- driver.sh
+++ driver.sh
@@ -1,2 +1,14 @@
-./LBT parameters.dat jet_shower_parton.dat lp-posi.dat lp-nega.dat
+#!/bin/bash
+
+#PBS -q C8
+#PBS -l select=1
+#PBS -N LBT_test
+#PBS -e errLBT.out 
+#PBS -o outLBT.out 
+#PBS -m abe
+#PBS -M yuuka.kanakubo@gmail.com
+
+cd $PBS_O_WORKDIR
+
+time ./LBT parameters.dat ../PYTHIA8/jet_shower_parton.dat lp-posi.dat lp-nega.dat
 
